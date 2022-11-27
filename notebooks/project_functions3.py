8d1abcb42ba6b20d54b3fc80afe99d8e5ad9da07
Anna Ciji Panakkal <annacp@student.ubc.ca>
2022-11-27 13:10:53 -0800

    moved clean data to processed data folder

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -2,27 +2,25 @@ import pandas as pd
 
 # Method Chain 1 (Load data, drop na rows, drop unneeded columns)
 def load_and_process(filePath):
-    unneededColumns = ['Stage', 'Win conditions', 'Half-time Home Goals', 'Half-time Away Goals', 'Referee', 'Assistant 1', 'Assistant 2', 'RoundID', 'MatchID', 'Home Team Initials', 'Away Team Initials']    
-     df1 = (
+    unneededColumns = ['Stage', 'Win conditions', 'Half-time Home Goals', 'Half-time Away Goals', 'Referee', 'Assistant 1', 'Assistant 2', 'RoundID', 'MatchID', 'Home Team Initials', 'Away Team Initials', 'Datetime', 'Stadium', 'City'] 
+    df1 = (
         pd.read_csv("../data/raw/WorldCupMatches.csv")
         .dropna()
         .drop(unneededColumns, axis=1)
+        .sort_values('Attendance', ascending=False)
         .reset_index(drop=True)
     )
- 
-
-
-# Method Chain 2 (adding column with values for winner)
+    
+    
+# Method Chain 2 (adding column with total goals) 
 def total_attendance (row):
     total_goals= cleanData['Home Team Goals'] + cleanData['Away Team Goals']    
     
     df1['Total Goals'] = (
         df1
-        .apply (lambda row: total_attendance(row), axis=1)
+        .apply(lambda row: total_attendance(row), axis=1)
     )
     return df1
-
-
 
 # Method Chain 3 (dropping all rows where year != 2014)
 def final_data(row):
